9cbabd19b700543cc0b12fd665a88fffcaa82857
teresabat <60197944+teresabat@users.noreply.github.com>
2024-06-30 19:56:06 -0300

    Update ex001.js
    Adicionei comentarios explicando as tres principais variaveis que vou utilizar durante o estudo, alem das constantes

--- ex001.js
+++ ex001.js
@@ -1,3 +1,10 @@
+// string: tudo que contém texto, palavras, geralmente dentro de aspas duplas
+// number: toda variável que trata número, seja ele int, decimal, float, não há necessidade de declarar qual o tipo de variável number no js, e não há necessidade de aspas para ser declarada
+// boolean: string para tratar verdadeiro ou falso ex: lula é ladrão? true ou falso?
+
+// const é abreviação de constantes, que não da para alterar o valor após ter sido declarado
+// variável é tudo aquilo que pode variar
+
 const btn = document.getElementById('btn')
 let p = document.getElementById('p')
 
